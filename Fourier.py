3eb27d83994c32a36b24c67185d8f6740a8e0c48
Maria Camila Paris <mariacamilaparis@MacBook-de-Juan.local>
2018-10-27 11:16:24 -0500

    2 commit

--- Fourier.py
+++ Fourier.py
@@ -1,7 +1,7 @@
 #Fourier
 import numpy as np
 import matplotlib.pylab as plt
-
+from scipy.fftpack import fft, fftfreq
 
 
 data_signal=np.genfromtxt("signal.dat", delimiter=",")
@@ -25,10 +25,14 @@ def Fourier(numpuntos,f):
     return fourier
 n=len(y) 
 a=Fourier(n,y)
+f=100
+dt = 1 / (f * 32 )
+freq = fftfreq(n, dt)
+
 plt.figure
-plt.xlabel("x")
+plt.xlabel("Frecuencia")
 plt.ylabel("T. de Fourier")
 plt.grid()
-plt.plot(x,a)
+plt.plot(freq,a, c="r")
 plt.savefig("ParisCamila_TF.pdf")
 # HW3
