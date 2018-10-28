8acf83c8ba810db2287896eecddba9d927d5803e
Maria Camila Paris <mariacamilaparis@MacBook-de-Juan.local>
2018-10-28 18:09:48 -0500

    2

--- PCR.py
+++ PCR.py
@@ -8,5 +8,21 @@ import numpy as np
 import matplotlib.pylab as plt
 
 data=np.genfromtxt("WDBC.dat", delimiter=",", replace_space='_',dtype="unicode")
+for i in range(len(data[0,:])):
+    for j in range(len(data[:,0])):
+        if(data[j,i]=="M"):
+            data[j,i]=1
+        elif(data[j,i]=="B"):
+            data[j,i]=0
+v0=data[:,0]
+v1=data[:,1]
+v2=data[:,2]
+v3=data[:,3]
+v=[]
+v.append(4)
+#for i in range(len(data[0,:])):
 
+
+print(np.mean(v0.astype(float)))
+#v0=(data[0,:]-np.mean(data[0,:]))/(np.sqrt(np.var(data[0,:])))
 # HW3
