13872ea0f47d81e19d466a43ef9c61d7f0dc1f03
Maria Camila Paris <mariacamilaparis@MacBook-de-Juan.local>
2018-10-29 18:35:06 -0500

    4

--- Fourier2D.py
+++ Fourier2D.py
@@ -3,30 +3,30 @@
 import numpy as np
 import matplotlib.pylab as plt
 from scipy.fftpack import fft, fftfreq
+from matplotlib.colors import LogNorm
 
 photo = plt.imread("arbol.png")
 f = np.fft.fft2(photo)
 fs= np.fft.fftshift(f)
-f1=np.fft.fftfreq(len(fs[0]))
-f2=np.fft.fftfreq(len(fs[1]))
-plt.imshow(np.log10(abs(fs)), cmap="gray")
+freq=np.fft.fftfreq(len(f[0]))
+plt.imshow((abs(f)), cmap="gray")
 plt.colorbar()
 plt.show()
-plt.plot(f1, abs(fs))
-plt.plot(f2, abs(fs))
+plt.plot(freq, abs(f))
 plt.show()
-cop=np.copy(fs)
 
-for i in range(np.shape(fs)[0]):
-	for j in range(np.shape(fs)[1]):
-		if(abs(cop[i,j])>4100 and abs(cop[i,j])<5000):
-			cop[i,j]=0.0
-plt.plot(f1, abs(cop))
-plt.plot(f2, abs(cop))
+for i in range(np.shape(f)[0]):
+	for j in range(np.shape(f)[0]):
+		if(abs(fs[i,j])>4100 and abs(fs[i,j])<5000):
+			fs[i,j]=0.0
+plt.imshow(np.abs(fs), norm=LogNorm(vmin=5))
 plt.show()
-img=np.fft.ifft2(cop)
+plt.plot(freq, abs(fs))
+plt.show()
+img=np.fft.ifft2(fs)
 plt.imshow(abs(img),cmap="gray")
 plt.show()
 #plt.savefig("ParisCamila_FT2D.pdf")
 #print(np.shape(photo))
 # HW3
+
