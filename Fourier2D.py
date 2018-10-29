08109bbe9004cdd20a583372264bf6ba30355a3a
Maria Camila Paris <mariacamilaparis@MacBook-de-Juan.local>
2018-10-29 10:19:14 -0500

    2

--- Fourier2D.py
+++ Fourier2D.py
@@ -5,14 +5,9 @@ import matplotlib.pylab as plt
 from scipy.fftpack import fft, fftfreq
 
 photo = plt.imread("arbol.png")
-x=photo[:,0]
-y=photo[:,1]
-n=len(y) 
-dt=x[1]-x[0]
-fft_x = np.abs(fft(y))
-freq = fftfreq(n, dt)
-plt.figure()
-plt.xlim(-0.75,0.75)
-plt.plot(freq,fft_x)
-plt.savefig("ParisCamila_FT2D.pdf")
+f = np.fft.fft2(photo)
+plt.imshow(abs(f), cmap="gray")
+plt.show()
+#plt.savefig("ParisCamila_FT2D.pdf")
+#print(np.shape(photo))
 # HW3
